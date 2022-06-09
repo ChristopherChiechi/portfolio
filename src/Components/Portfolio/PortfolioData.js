f97253db879c3fb74ab63f47e30d3414ae01372d
Chiechi, Christopher <christopherchiechi@my.unt.edu>
2022-06-09 14:28:26 -0500

    Updated portfolio links and blog

--- src/Components/Portfolio/PortfolioData.js
+++ src/Components/Portfolio/PortfolioData.js
@@ -11,7 +11,7 @@ const PortfolioData = [
     category: "Game Development",
     title: "Parkinson's Research Study",
     image: PerfectDecision,
-    link: "https://christopher-chiechi.itch.io/parkinsons-game?secret=sP4rE9NA2SapNq0lxWT7kAb8BRc",
+    link: "https://github.com/ChristopherChiechi/ParkinsonsGame",
     desc: "A video game test to evaluate patients for mild cognitive impairment (MCI), which is common in patients with Parkinson's disease (PD). A research study is being conducted at Florida State University to evaluate 90 participants (30 with PD and MCI, 30 with PD and without MCI, and 30 without PD) using both traditional and video game modalities. The study aims to determine the potential video games present as an effective tool for future clinical examinations to detect early signs of MCI in individuals with PD. Made with Unity.",
   },
   {
@@ -19,7 +19,7 @@ const PortfolioData = [
     category: "Game Development",
     title: "First Person Shooter",
     image: ColorRunner,
-    link: "https://christopher-chiechi.itch.io/color-runner?secret=Gkq4W6ZbYllQsO2eqpF1SUoe4",
+    link: "https://github.com/ChristopherChiechi/FirstPersonShooter",
     desc: "A first-person shooter time trial made in Unreal Engine. The player must navigate the course by sprinting, double jumping, wall running, sliding, and crouching. The player must also switch between four weapons of differing types and shoot moving targets of the same color as their weapon. Three seconds are added for every target missed, and the best time is saved.",
   },
   {
@@ -27,8 +27,7 @@ const PortfolioData = [
     category: "Game Development",
     title: "Audio Game Experiment",
     image: BlindMazeGame,
-    //link: "https://github.com/ChristopherChiechi/BlindMazeGame",
-    link: "https://christopher-chiechi.itch.io/blind-maze-game",
+    link: "https://github.com/ChristopherChiechi/BlindMazeGame",
     desc: "This game is an experiment to determine how effective audio cues are in guiding the player. Half of the levels will be played with visuals only, while the other half will be played with audio only. The results will help to evaluate the effectiveness of audio cues in both normal games and audio games for the blind. Results are posted to a Firebase database for comparison. Made with Unity.",
   },
   {
