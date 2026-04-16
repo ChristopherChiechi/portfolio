f8df389ef14098aa9ae9c301909eb63bb326a51e
Christopher Chiechi <chiechichris@gmail.com>
2026-04-15 23:23:22 -0500

    update(skills): Added Java

--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -132,6 +132,18 @@ const items = [
 		category: 'pro-lang'
 	}),
 	defineSkill({
+		slug: 'java',
+		color: '#4949f1',
+		description:
+			`Java is a widely-used programming language for coding web applications. It has been a popular choice among developers for over two decades,
+			with millions of Java applications in use today. Java is a multi-platform, object-oriented, and network-centric language that can be used as
+			a platform in itself. It is a fast, secure, reliable programming language for coding everything from mobile apps and enterprise software to
+			big data applications and server-side technologies. — Amazon Web Services`,
+		logo: Assets.Java,
+		name: 'Java',
+		category: 'pro-lang'
+	}),
+	defineSkill({
 		slug: 'sql',
 		color: 'orange',
 		description:
