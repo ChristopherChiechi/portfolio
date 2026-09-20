733a660e3c8e9504f9b008ef77b04ebe803459f1
ChristopherChiechi <chiechichris@gmail.com>
2026-09-20 05:57:33 +0000

    Deploying to gh-pages from @ ChristopherChiechi/portfolio@343f896d5c7f74ca4430b2a6e84f28902a4d3386 🚀

--- _app/immutable/chunks/experience.CJ8FbuaU.js
+++
@@ -1,6 +0,0 @@
-import{A as i}from"./assets.BVPsFUTk.js";import{a}from"./skills.CcEB8iny.js";var t=(e=>(e.FullTime="Full-time",e.PartTime="Part-time",e.SelfEmployed="Self-employed",e.Freelance="Freelance",e.Contract="Contract",e.Internship="Internship",e))(t||{});const o="Experience",n=[{slug:"lockheed-martin-software-engineer",company:"Lockheed Martin Aeronautics",description:`I have experience working on a cross-functional team developing many 
-		concurrent projects that span the full development lifecycle, collaborating with teams 
-		across labs and flight test to meet the critical needs of aeronautics programs. 
-		My team's work involves software engineering, data engineering, data science, and analysis. 
-		I have developed and contributed to projects that cover a wide range of aircraft capabilities. 
-		and facilitate multi-million dollar, mission critical decisions.`,contract:t.FullTime,type:"Software Development",location:"Fort Worth",period:{from:new Date("2023-06-05")},skills:a("python","pandas","plotly"),name:"Software Engineer",color:"dodgerblue",links:[],logo:i.Lockheed,shortDescription:""}],s={title:o,items:n};export{s as E};
---
+++ _app/immutable/entry/start.e2wLJQvU.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.d0Pkum1J.js";export{t as start};
